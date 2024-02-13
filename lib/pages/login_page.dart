02c9418c707e2bfb356c5aa27e72b9dcf5598209
jagmia2 <jagmia2@gmail.com>
2024-02-14 00:54:40 +0100

    1st commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,61 @@
+import 'package:chat_app/components/my_text_field.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  //text controller
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: Column(
+              children: [
+                //logo
+                Icon(
+                  Icons.message,
+                  size: 80,
+                  color: Colors.grey[800],
+                ),
+                //welcome back message
+                const Text(
+                  "Welcome back you\'ve been missed",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black87,
+                  ),
+                ),
+                //email textfield
+                MyTextField(
+                  controller: emailController,
+                  hintText: 'Email',
+                  obscureText: false,
+                ),
+                //password textfield
+                MyTextField(
+                  controller: passwordController,
+                  hintText: 'Password',
+                  obscureText: false,
+                ),
+                //sign in button
+
+                //not a member? register now
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
