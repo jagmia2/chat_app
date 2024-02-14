8d68b0200a2c6cbfc8556ef6f34817fd0cb11d6e
jagmia2 <jagmia2@gmail.com>
2024-02-14 11:04:10 +0100

    login button added

---
+++ lib/components/my_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final void Function()? onTap;
+  final String text;
+  const MyButton({
+    super.key,
+    required this.onTap,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(25),
+        decoration: BoxDecoration(
+            color: Color(0xFF1FDB5F), borderRadius: BorderRadius.circular(10)),
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/components/my_button.dart';
 import 'package:chat_app/components/my_text_field.dart';
 import 'package:flutter/material.dart';
 
@@ -22,34 +23,39 @@ class _LoginPageState extends State<LoginPage> {
             padding: const EdgeInsets.symmetric(horizontal: 25),
             child: Column(
               children: [
+                const SizedBox(height: 50),
                 //logo
                 Icon(
                   Icons.message,
                   size: 80,
-                  color: Colors.grey[800],
+                  color: Color(0xFF1FDB5F),
                 ),
+                const SizedBox(height: 50),
                 //welcome back message
                 const Text(
-                  "Welcome back you\'ve been missed",
+                  "Добро пожаловать!",
                   style: TextStyle(
                     fontSize: 16,
                     color: Colors.black87,
                   ),
                 ),
+                const SizedBox(height: 25),
                 //email textfield
                 MyTextField(
                   controller: emailController,
                   hintText: 'Email',
                   obscureText: false,
                 ),
+                const SizedBox(height: 10),
                 //password textfield
                 MyTextField(
                   controller: passwordController,
-                  hintText: 'Password',
+                  hintText: 'Пароль',
                   obscureText: true,
                 ),
+                const SizedBox(height: 20),
                 //sign in button
-
+                MyButton(onTap: () {}, text: "Войти")
                 //not a member? register now
               ],
             ),
